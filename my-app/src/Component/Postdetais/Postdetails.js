659ef290656677fbf20b52ec99fecc9613249195
Foysalrabby <81823837+Foysalrabby@users.noreply.github.com>
2023-07-11 23:02:40 +0600

    strat work

--- my-app/src/Component/Postdetais/Postdetails.js
+++ my-app/src/Component/Postdetais/Postdetails.js
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import'./Postdetails.css';
 const Postdetails = (props) => {
     const {title,body}=props.post;
     return (
-        <div>
+        <div className="postdetail">
             <h1>{title}</h1>
             <p>{body}</p>
+            <button>show post</button>
         </div>
     );
 };
